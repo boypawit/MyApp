fb3378c8c4f692de723b9bbe121b886520bd9511
boypawit <boyvinai@windowslive.com>
2017-06-07 15:16:05 +0700

    first commit

--- app/src/main/java/com/example/boyvi/myapp/MainActivity.java
+++ app/src/main/java/com/example/boyvi/myapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.boyvi.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void showtext(View view){
+        String msg = "Hello, I am Pawit Termjukul";
+        Toast toast = Toast.makeText(this,msg,Toast.LENGTH_SHORT);
+        toast.show();
+
     }
 }
